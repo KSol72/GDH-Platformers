e4bc8696dc878cb7ef80f6bd2ea59dd0ac8dffd2
KSol72 <143913267+KSol72@users.noreply.github.com>
2024-06-06 10:32:33 +0000

    url actually updated now lol

--- htdocs/nav.php
+++ htdocs/nav.php
@@ -1,5 +1,5 @@
 <ul class="nav">
-  <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "changelog"){echo 'active-nav-item';}?>" href="https://demonlist.gamersdreamhouse.repl.co/">Back to Demonlist</a></li>
+  <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "changelog"){echo 'active-nav-item';}?>" href="https://gdhdemonlist.hpsk.me/index.php">Back to Demonlist</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="leaderboard.php">Leaderboard</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "index"){echo 'active-nav-item';}?>" href="index.php">Main List</a></li>
     <li class="titlebox"><a href="homepage.php" class="titlebox-href">GDH: Platformer List</a></li>
