af37e2f05584dafb16f8cc46d2d034678ff076f2
KSol72 <143913267+KSol72@users.noreply.github.com>
2024-06-06 10:18:47 +0000

    demonlist redirect url update

--- htdocs/nav.php
+++ htdocs/nav.php
@@ -8,7 +8,7 @@
         <div id="myDropdown" class="dropdown-content">
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "index"){echo 'active-nav-item';}?>" href="index.php">Main List</a>
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="leaderboard.php">Leaderboard</a>
-    <a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="https://demonlist.gamersdreamhouse.repl.co/">Back to Demonlist</a></li>
+    <a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="https://gdhdemonlist.hpsk.me/index.php">Back to Demonlist</a></li>
 </div>
     </li>
 </ul>
